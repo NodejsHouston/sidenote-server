ab6a0ce0a70dc85ee1e91058963806e2b32d4f88
Nolan Prescott <prescott.nolan@gmail.com>
2016-03-18 19:16:41 -0500

    Appease the linter
    There are 15 errors outstanding, all related to generating mock
    data. Ignoring them along with an error involving the rootRequire
    function in global scope (eventually this will be factored out).

--- server/base/index.js
+++ server/base/index.js
@@ -1,24 +1,23 @@
 var Boom = require('boom');
 
 // Base routes for default index/root path, about page, 404 error pages, and others..
-exports.register = function(server, options, next){
+exports.register = function(server, options, next) {
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      config: {
+        handler: function(request, reply) {
+          reply(Boom.notFound());
+        },
+        id: 'index'
+      }
+    }
+  ]);
 
-    server.route([
-        {
-            method: 'GET',
-            path: '/',
-            config: {
-                handler: function(request, reply){
-                    reply(Boom.notFound());
-                },
-                id: 'index'
-            }
-        }
-    ]);
-
-    next();
-}
+  next();
+};
 
 exports.register.attributes = {
-    name: 'base'
+  name: 'base'
 };
